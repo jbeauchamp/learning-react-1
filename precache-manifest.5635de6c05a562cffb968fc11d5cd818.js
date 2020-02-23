14a1aab8453796b5313cc1214d0b5e10a0b67edb
James Beauchamp <james.beauchamp@futurumllc.com>
2020-02-23 11:36:53 -0700

    Updates

---
+++ precache-manifest.5635de6c05a562cffb968fc11d5cd818.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d47a62fa7dce0cf75e3e3391e9b2f856",
+    "url": "/learning-react-1/index.html"
+  },
+  {
+    "revision": "cc725d6143e94802fb78",
+    "url": "/learning-react-1/static/css/main.62a9e2d8.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/learning-react-1/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/learning-react-1/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc725d6143e94802fb78",
+    "url": "/learning-react-1/static/js/main.331e0a77.chunk.js"
+  },
+  {
+    "revision": "845210b90e10307c82f7",
+    "url": "/learning-react-1/static/js/runtime-main.3e547e02.js"
+  }
+]);
